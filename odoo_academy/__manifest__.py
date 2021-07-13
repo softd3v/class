b66b629178e1f1c8eebebf7cf3ea0473d0167adc
Emmanuel Paternina <e.paternina@softd3v.com>
2021-07-13 03:08:46 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# .*. coding: utf-8 .*.
+
+{
+    'name': 'Odoo Academy',
+    
+    'sumary': """Academy app to manage Trainig""",
+    
+    'description': """
+        Academy Modula to Manage Taining:
+        - Courses
+        - Sessions
+        - Attendess
+    """,
+    'author': 'Odoo',
+    
+    'webiste': 'https://.www.odoo.com',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
